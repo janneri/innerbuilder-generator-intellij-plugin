3dc8b0cb283f0e94e2743b5608e3a27f5d7475c0
GitHub Action <action@github.com>
2020-12-10 18:41:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/janneri/innerbuildergeneratorintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.janneri.innerbuildergeneratorintellijplugin.services
+
+import com.github.janneri.innerbuildergeneratorintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/janneri/innerbuildergeneratorintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/janneri/innerbuildergeneratorintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.janneri.innerbuildergeneratorintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.janneri.innerbuildergeneratorintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
